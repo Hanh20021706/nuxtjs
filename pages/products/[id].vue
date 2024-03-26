bb15645227de227c5d04ba837809a0e421037d3c
Hanh20021706 <hanhs8155@gmail.com>
2024-03-26 16:48:33 +0700

    feat: show detail product

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,12 +1,19 @@
 <template>
   <div>
+    <h2>{{ product.title }}</h2>
     <h2>this is product detail anh id: {{ id }}</h2>
+    <img alt="" :src="product.image" class="w-[100px] h-[100px]" />
+    <p>{{ product.price }}</p>
   </div>
 </template>
 
 <script setup>
 // sử dụng useRoute đẻ lấy được id của sản phẩm
 const { id } = useRoute().params;
+
+const uri = "https://fakestoreapi.com/products/" + id;
+
+const { data: product } = await useFetch(uri, { key: id });
 
 // sau đó gọi chúng ở đây để sử dụng
 definePageMeta({
