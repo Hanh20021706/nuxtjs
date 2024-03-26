ead7f9b37e9d166ff337e244d7e92e78c18ed5f2
Hanh20021706 <hanhs8155@gmail.com>
2024-03-26 14:32:05 +0700

    feat: create route

---
+++ pages/products/index.vue
@@ -0,0 +1,12 @@
+/** khi tạo 1 page chỉ cần tạo trong thư mục pages đã tạo ở ngoài thì tên thư
+mục hoặc tên file sẽ là đường dẫn ( route ) của trang đó */
+
+<template>
+  <div>
+    <h2>this is product page</h2>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
