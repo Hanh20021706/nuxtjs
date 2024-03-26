275a7c7261464669dbb00ad3d821fce5963f3d5b
Hanh20021706 <hanhs8155@gmail.com>
2024-03-26 16:25:06 +0700

    feat: use useFetch call api

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,13 +1,27 @@
-/** khi tạo 1 page chỉ cần tạo trong thư mục pages đã tạo ở ngoài thì tên thư
-mục hoặc tên file sẽ là đường dẫn ( route ) của trang đó */
-
 <template>
   <div>
     <h2>this is product page</h2>
     <button class="button">click me</button>
+
+    <!-- hiển thị danh sách sản phẩm -->
+    <h2>hiển thị danh sách sản phẩm</h2>
+    <div class="flex flex-wrap gap-2">
+      <!-- dùng for để list ra sản phẩm  -->
+      <div class="border border-gray-500 p-3" v-for="product in products">
+        <!-- dùng nuxtlink để click vào trang chi tết sản phẩm  -->
+        <NuxtLink :to="`/products/${product.id}`">
+          <h2>
+            {{ product.title }}
+          </h2>
+        </NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+//fetch data
+const { data: products } = await useFetch("https://fakestoreapi.com/products");
+</script>
 
 <style scoped></style>
