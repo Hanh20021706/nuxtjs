d598dc1b6af234aa9dfe03eb64b8f3548ecd75a1
Hanh20021706 <hanhs8155@gmail.com>
2024-03-26 14:38:44 +0700

    feat: create route id

---
+++ pages/products/[id].vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>this is product detail anh id: {{ id }}</h2>
+  </div>
+</template>
+
+<script setup>
+// sử dụng useRoute đẻ lấy được id của sản phẩm
+const { id } = useRoute().params;
+</script>
+
+<style scoped></style>
