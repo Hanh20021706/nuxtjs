e17a85c7176714b943362d7d91f3cbfc4ebad347
Hanh20021706 <hanhs8155@gmail.com>
2024-03-26 14:27:53 +0700

    feat: first commit

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <div class="">hello</div>
+  <p>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt beatae
+    laboriosam iste, incidunt quam accusantium assumenda? Quaerat totam eius
+    ipsam! Cum dolorem quidem tempore a dignissimos distinctio maiores odio sed!
+  </p>
+</template>
