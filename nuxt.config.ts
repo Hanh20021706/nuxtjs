ce817e89c6b35533202239533ce19cb9d2679074
Hanh20021706 <hanhs8155@gmail.com>
2024-03-27 14:29:26 +0700

    feat: seo and meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
+  // SEO và meta
+  app: {
+    head: {
+      // tiêu đề hiển thị
+      title: "edu form",
+      // phần meta của thẻ head
+      meta: [{ name: "description", content: "My amazing site." }],
+    },
+  },
 });
